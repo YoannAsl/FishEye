71c4e50f316d95312e3fc50a15938aa370365c9d
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2020-11-22 20:46:52 +0100

    Ajout tri par tag page d'accueil + retiré librairie lightbox

--- index.js
+++ index.js
@@ -1,47 +1,87 @@
-const cardsContainer = document.querySelector(".cards_container");
-const tagsContainer = document.querySelector(".tags");
+class Index {
+    constructor()  {
+        this.phList = [];
+        this.ajax = new Ajax;
+        
+        this.cardsContainer = document.querySelector(".cards_container");
+        this.tagsContainer = document.querySelector(".tags");
+    }
 
-const getData = async () => {
-    try {
-        const res = await axios.get("./data.json");
-        return res.data;
-    } catch(e) {
-        console.log(e);
+    // async createCards() {
+    //     const data = await this.ajax.getData();
+    //     data.photographers.forEach(e => {
+    //         let listTag = "" ;
+    //         e.tags.map(e => {
+    //             listTag += `<a class="tag ${e}" href="#"><span>#${e}</span></a>`;
+    //         });
+
+    //         const newCard = document.createElement("div");
+
+    //         let url = "./photographer.html";
+    //         url = url + "?id=" + e.id;
+    //         newCard.className = "card";
+    //         newCard.innerHTML = `
+    //             <a href="${url}">
+    //                 <img src="images/Photographers_ID_Photos/${e.portrait}">
+    //                 <h2>${e.name}</h2>
+    //             </a>
+    //             <p class="location">${e.city}, ${e.country}</p>
+    //             <p>${e.tagline}</p>
+    //             <p class="price">${e.price}€/jour</p>
+    //             <div class="tags">
+    //                 ${listTag}
+    //             </div>
+    //         `;
+    //         this.cardsContainer.append(newCard);
+    //     });
+    // }
+    async init() {
+        const data = await this.ajax.getData();
+        data.photographers.forEach(e => {
+            this.phList.push(e);
+        });
+        this.createCards()
+    }
+
+    createCards() {
+        this.phList.forEach(e => {
+            let listTag = "" ;
+            e.tags.map(e => {
+                listTag += `<a class="tag ${e}" href="#"><span>#${e}</span></a>`;
+            });
+
+            const newCard = document.createElement("div");
+
+            let url = "./photographer.html";
+            url = url + "?id=" + e.id;
+            
+            newCard.className = "card";
+            newCard.innerHTML = `
+                <a href="${url}">
+                    <img src="images/Photographers_ID_Photos/${e.portrait}">
+                    <h2>${e.name}</h2>
+                </a>
+                <p class="location">${e.city}, ${e.country}</p>
+                <p>${e.tagline}</p>
+                <p class="price">${e.price}€/jour</p>
+                <div class="tags">
+                    ${listTag}
+                </div>
+            `;
+            this.cardsContainer.append(newCard);
+        })
+    }
+
+    async updatePhList(tag) {
+        const data = await this.ajax.getData();
+        this.phList = [];
+
+        data.photographers.forEach(e => {
+            if (e.tags.includes(tag.toLowerCase())) {
+                this.phList.push(e);
+            }
+        });
+        this.cardsContainer.innerHTML = "";
+        this.createCards();
     }
 }
-
-const createCards = async () => {
-    const data = await getData();
-    data.photographers.forEach(e => {
-        let listTag = "" ;
-        e.tags.map(e => {
-            listTag += `<a class="tag ${e}" href="#"><span>#${e}</span></a>`;
-        });
-
-        const newCard = document.createElement("div");
-
-        let url = "./photographer.html";
-        url = url + "?id=" + e.id;
-        newCard.className = "card";
-        newCard.innerHTML = `
-            <a href="${url}">
-                <img src="images/Photographers_ID_Photos/${e.portrait}">
-                <h2>${e.name}</h2>
-            </a>
-            <p class="location">${e.city}, ${e.country}</p>
-            <p>${e.tagline}</p>
-            <p class="price">${e.price}€/jour</p>
-            <div class="tags">
-                ${listTag}
-            </div>
-        `;
-        cardsContainer.append(newCard);
-    });
-}
-
-
-// const sortByTag = tag => {
-//     if (tag.className ==! )
-// }
-
-createCards();
